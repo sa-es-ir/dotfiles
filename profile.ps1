88c372fefbf8e21868363cdb1debd25c279cf3a9
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-08-03 23:00:51 +0700

    add font update

--- profile.ps1
+++ profile.ps1
@@ -8,6 +8,21 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | 
 
 oh-my-posh font install meslo
 
+#Once you have installed a Nerd Font, you will need to configure the Windows Terminal to use it. This can be easily done by modifying the Windows Terminal settings (default shortcut: CTRL + SHIFT + ,). In your settings.json file, add the font.face attribute under the defaults attribute in profiles:
+
+#{
+#    "profiles":
+#    {
+#        "defaults":
+#        {
+#            "font":
+#            {
+#                "face": "MesloLGM Nerd Font"
+#            }
+#        }
+#    }
+#}
+
 ./.exports.ps1
 ./.aliases.ps1
 
