4f1b7ccc8188eba7df5308d535036851db90cb62
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-08-03 23:00:29 +0700

    fix installation

--- install.ps1
+++ install.ps1
@@ -1,6 +1,7 @@
 echo hi
 
-# needs install choco first
+# needs install choco first by running blow command
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 #choco feature enable -n=allowGlobalConfirmation
 
@@ -12,10 +13,11 @@ echo hi
 
 #choco feature disable -n=allowGlobalConfirmation
 
-#Install-Module -Name z -AllowClobber
+Install-Module -Name z -AllowClobber
 Install-Module -Name posh-git
 Install-Module -Name PowerShellGet -Force
 Update-Module PowerShellGet -Force
 Install-Module -Name PSReadLine -Force
+# close and open the terminal
 Update-Module PSReadLine -Force
 Install-Module -Name Terminal-Icons
